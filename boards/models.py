22b92b54ad0be3667d644cbfe76d2ffcb2137dba
Kim-Seul-Gi <rlatmf2147@naver.com>
2019-03-19 00:27:01 +0000

    04|migration|

--- boards/models.py
+++ boards/models.py
@@ -6,8 +6,8 @@ class Board(models.Model):
     title = models.CharField(max_length = 10)
     content = models.TextField()
     hit = models.IntegerField(default=0)
-    created_at = models.DateTiemField(auto_now_add=True)
-    updated_at = models.DateTiemField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'<Board ({self.id})> : {self.title}'
